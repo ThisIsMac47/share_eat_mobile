a5e13308a30bfd188b6539b54805b760f4d71216
Valentin “ThisIsMac” M <thisismac47@gmail.com>
2016-03-23 23:51:17 +0100

    next step modal meetup

--- app/pages/invitation/invitation.ts
+++ app/pages/invitation/invitation.ts
@@ -1,5 +1,6 @@
 import {IonicApp, Page, NavController, Events, MenuController} from 'ionic-angular';
 import {HomePage} from '../home/home';
+import * as _ from 'lodash';
 
 import {HttpService} from '../../providers/http-service';
 
@@ -9,8 +10,11 @@ import {HttpService} from '../../providers/http-service';
 })
 export class InvitationPage {
 
-  waitingInvitations: any;
-  oldInvitations: any;
+  // all datas
+  waitingInvitations = Array<any>();
+  oldInvitations = Array<any>();
+  futureMeetups = Array<any>();
+
   stats: Object;
   loading = false;
 
@@ -21,12 +25,36 @@ export class InvitationPage {
     this.stats = { "received": 0, "futur": 0, "sent": 0};
     this.loading = true;
 
+    // Preload the number
     http.makeBackendRequest('GET', 'me/stats', null, response => {
         this.stats = response;
-        this.loading = false;
+        this.updateData();
     }, errorMessage => {
-      this.loading = false;
+        this.loading = false;
         HttpService.showAlert(this.nav, "Error code : " + errorMessage.status, "Notre serveur n'a pas répondu, veuillez réessayez", "Ok");
+    }, true);
+
+  }
+
+  updateData() {
+    let tmp: Array<any>;
+    this.http.makeBackendRequest('GET', 'me/invitations/WAITING', null, response => {
+      tmp = response;
+      for(let i = 0; i < response.length; i++) {
+
+        this.http.makeBackendRequest('GET', 'meetup/show/' + response[i].meetup, null, res => {
+            tmp[i].meetup = res;
+            tmp[i].meetup.date = _.replace(tmp[i].meetup.date, 'T', ' at ');
+            this.http.makeBackendRequest('GET', 'location/show/' + res.location.id, null, re => {
+                tmp[i].meetup.location = re;
+                this.waitingInvitations.push(tmp[i]);
+                this.loading = false;
+            }, errorMessage => {  }, true);
+        }, errorMessage => {  }, true);
+
+      }
+    }, errorMessage => {
+      HttpService.showAlert(this.nav, "Error code : " + errorMessage.status, "Notre serveur n'a pas répondu, veuillez réessayez", "Ok");
     }, true);
   }
 
